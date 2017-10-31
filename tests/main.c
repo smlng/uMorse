783ae2191d276c7fcc21a5a2338f907e93b0037c
smlng <s@mlng.net>
2017-10-31 01:30:18 +0100

    tests: enhance core test

--- tests/main.c
+++ tests/main.c
@@ -45,20 +45,28 @@ int test_umorse_print(void)
 {
 	setbuf(stdout, NULL);
 	uint8_t code[CODE_LEN];
+	int ret = -1;
 	printf("> Translate text of length %lu into Morse code:\n\"%s\"\n",
 		   strlen(text), text);
 
 	memset(code, 0, CODE_LEN);
 	printf("> using aligned encoding:\n");
-	int ret1 = umorse_encode_aligned(text, sizeof(text), code, sizeof(code));
-	umorse_output(&out, code, ret1, 0x0);
-	printf("> encoded length=%d\n", ret1);
+	ret = umorse_encode_aligned(text, sizeof(text), code, sizeof(code));
+	if (ret < 0) {
+		return 1;
+	}
+	umorse_output(&out, code, ret, 0x0);
+	printf("> encoded length=%d\n", ret);
 
 	memset(code, 0, CODE_LEN);
-	int ret2 = umorse_encode_compact(text, sizeof(text), code, sizeof(code));
 	printf("> using compact encoding:\n");
-	umorse_output(&out, code, ret2, 0x0);
-	printf("> encoded length=%d\n", ret2);
+	ret = umorse_encode_compact(text, sizeof(text), code, sizeof(code));
+	if (ret < 0) {
+		return 2;
+	}
+	umorse_output(&out, code, ret, 0x0);
+	printf("> encoded length=%d\n", ret);
+
 	return 0;
 }
 
